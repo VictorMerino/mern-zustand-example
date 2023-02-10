c95416a79c61115a21af65bac3ad596849ce97cb
Víctor Merino <victor.merino@jungheinrich.es>
2023-02-10 14:39:03 +0100

    Verify jwt token

--- server/middlewares/requireAuth.ts
+++ server/middlewares/requireAuth.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
 
 export const requireAuth = (
   req: Request,
@@ -6,6 +7,18 @@ export const requireAuth = (
   next: NextFunction
 ) => {
   const authHeader = req.headers.authorization
-  if (!authHeader) return res.status(401).json({ message: 'Unauthorized' })
-  next()
+  if (!authHeader)
+    return res.status(401).json({ message: 'Unauthorized (no Auth header)' })
+  const [bearerString, token] = authHeader.split(' ')
+  if (bearerString !== 'Bearer')
+    return res.status(401).json({ message: 'Unauthorized (no Bearer)' })
+
+  console.log({ bearerString, token })
+
+  verify(token, 'secret-fafdasdfadsdfas', (err, user) => {
+    if (err)
+      return res.status(401).json({ message: 'Unauthorized (bad secret)' })
+    console.log(user)
+    next()
+  })
 }

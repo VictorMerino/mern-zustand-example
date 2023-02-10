4c8537a361412e92add7105a5dff8812b903904d
Víctor Merino <victor.merino@jungheinrich.es>
2023-02-10 15:00:14 +0100

    Avoid magical number for authorization error

--- server/middlewares/requireAuth.ts
+++ server/middlewares/requireAuth.ts
@@ -7,18 +7,21 @@ export const requireAuth = (
   next: NextFunction
 ) => {
   const authHeader = req.headers.authorization
+
+  let status = 401
   if (!authHeader)
-    return res.status(401).json({ message: 'Unauthorized (no Auth header)' })
+    return res.status(status).json({ message: 'Unauthorized (no Auth header)' })
   const [bearerString, token] = authHeader.split(' ')
   if (bearerString !== 'Bearer')
-    return res.status(401).json({ message: 'Unauthorized (no Bearer)' })
+    return res.status(status).json({ message: 'Unauthorized (no Bearer)' })
 
   console.log({ bearerString, token })
 
   verify(token, 'secret-fafdasdfadsdfas', (err, user) => {
     if (err)
-      return res.status(401).json({ message: 'Unauthorized (bad secret)' })
-    console.log(user)
+      return res.status(status).json({ message: 'Unauthorized (bad secret)' })
+
+    status = 200
     next()
   })
 }
